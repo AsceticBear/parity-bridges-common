515e99207294511a3677abd60745abf35dcea049
ouwenkg <2630582710@qq.com>
2020-12-10 10:08:06 +0800

    add scripts

--- scripts/run-millau-node.sh
+++ scripts/run-millau-node.sh
@@ -3,14 +3,14 @@
 # Run a development instance of the Substrate bridge node.
 
 # RUST_LOG=runtime=trace,rpc=debug \
-#     ./target/debug/rialto-bridge-node --dev --tmp \
+#     ./target/debug/millau-bridge-node --dev --tmp \
 #     --rpc-cors=all --unsafe-rpc-external --unsafe-ws-external
 
 # bear
 RUST_LOG=runtime=trace,rpc=debug \
-    ./target/debug/rialto-bridge-node  \
+    ./target/debug/millau-bridge-node \
         --dev --tmp \
-        --port 30334 \
-        --ws-port 9946 \
-        --rpc-port 9934 \
+        --port 30333 \
+        --ws-port 9945 \
+        --rpc-port 9933 \
         --rpc-cors=all --unsafe-rpc-external --unsafe-ws-external
---
+++ scripts/run-millau-to-rialto-relay.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# substrate-relay 0.1.0
+# Substrate-to-Substrate relay
+
+# USAGE:
+#     substrate-relay <SUBCOMMAND>
+
+# FLAGS:
+#     -h, --help       Prints help information
+#     -V, --version    Prints version information
+
+# SUBCOMMANDS:
+#     help                                          Prints this message or the help of the given subcommand(s)
+#     initialize-millau-headers-bridge-in-rialto    Initialize Millau headers bridge in Rialto
+#     initialize-rialto-headers-bridge-in-millau    Initialize Rialto headers bridge in Millau
+#     millau-headers-to-rialto                      Relay Millau headers to Rialto
+#     millau-messages-to-rialto                     Serve given lane of Millau -> Rialto messages
+#     rialto-headers-to-millau                      Relay Rialto headers to Millau
+#     rialto-messages-to-millau                     Serve given lane of Rialto -> Millau messages
+#     submit-millau-to-rialto-message               Submit message to given Millau -> Rialto lane
+#     submit-rialto-to-millau-message               Submit message to given Rialto -> Millau lane
+
+
+# ./target/debug/substrate-relay initialize-millau-headers-bridge-in-rialto \
+#     --millau-host 127.0.0.1 \
+#     --millau-port 9945 \
+#     --rialto-host 127.0.0.1 \
+#     --rialto-port 9946 \
+#     --rialto-signer //Alice
+
+# sleep 6
+
+RUST_LOG=substrate-relay=debug \
+    ./target/debug/substrate-relay millau-headers-to-rialto \
+	--millau-host 127.0.0.1 \
+	--millau-port 9945 \
+	--rialto-host 127.0.0.1 \
+	--rialto-port 9946 \
+	--rialto-signer //Alice \
+	--prometheus-host=0.0.0.0
+
+
+
+
